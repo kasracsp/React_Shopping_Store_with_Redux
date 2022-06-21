65dc697400f841c3832fa57e66f341ad917f3fa6
kasracsp <kasracaspian@gmail.com>
2022-06-22 04:23:31 +0430

    image magnifier set and some quick style added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import Loading from './Loading'
 import styles from './Product.module.css'
+import {Magnifier,SideBySideMagnifier} from "react-image-magnifiers";
 
 const Product = () => {
   const params=useParams()
   const productsState=useSelector(state=>state.productsState)
+
   const productData=productsState.products.filter(item=>item.id === Number(params.id))
-  const {title,image}=productData[0]
 
   return (
     <div className={styles.container}>
         {
-          productData.length ?
+          productData.length &&
           <div className={styles.productSection}>
-            <p>{title}</p>
-            <img src={image} alt="d" />
-          </div>:
-          <Loading />
+            <div className={styles.productThumb}>
+              <Magnifier
+                imageSrc={productData[0].image}
+                imageAlt={productData[0].title}
+                className={styles.Magnifier}
+              />
+              <SideBySideMagnifier
+                imageSrc={productData[0].image}
+                fillAvailableSpace={false}
+                imageAlt={productData[0].title}
+                overlayBoxColor='#fff'
+                overlayBackgroundColor='black'
+                inPlaceMinBreakpoint={768}
+                className={styles.sideBySide}
+              />
+            </div>
+            <div className={styles.productDetails}>
+              <h5>{productData[0].title}</h5>
+            </div>
+          </div>
         }
     </div>
   )
